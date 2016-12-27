9d012370cb926a8d8333889de80ff908b431e4d3
pradeepgain <pgowrishetty@sysgain.com>
2016-12-27 12:13:26 +0530

    upda

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var websocket;
 var messages = document.getElementById('docker-logs');
-debugger;
-websocket = new WebSocket('ws://13.93.148.138:4243/containers/188e2317e233/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
+//debugger;
+websocket = new WebSocket('ws://13.93.148.138:4243/containers/ddc151d0f218/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
 websocket.onopen = function(e) {
-    messages.innerHTML += "<p>Connected</p>";
-}
-debugger;
+        messages.innerHTML += "<p>Connected</p>";
+    }
+    //debugger;
 websocket.onmessage = function(e) {
     messages.innerHTML += "<p>" + e.data + "</p>";
 
