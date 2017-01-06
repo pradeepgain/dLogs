74e6742e6a019044c9e3c4b1dd7f4f8470491fd5
pradeepgain <pgowrishetty@sysgain.com>
2017-01-06 12:36:51 +0530

    ipupdated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var websocket;
 var messages = document.getElementById('docker-logs');
 //debugger;
-websocket = new WebSocket('ws://13.93.148.138:4243/containers/ddc151d0f218/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
+websocket = new WebSocket('ws://40.118.212.137:4243/containers/ddc151d0f218/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1');
 websocket.onopen = function(e) {
         messages.innerHTML += "<p>Connected</p>";
     }
